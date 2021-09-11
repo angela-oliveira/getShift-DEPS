c03209a4e7f8ab3f2efa2ce4e76b74b36c3ef8fa
andreia23 <andreialima.berto08@gmail.com>
2021-09-11 17:42:41 -0300

    iniciando o back com java

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # getShift-DEPS
+
 Projeto da disciplina Desenvolvimento e Execução de Projeto de Software do curso TSI do IFPB
---
+++ src/main/java/com/ifpb/deps/getshiftback/GetShiftBackApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.deps.getshiftback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetShiftBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetShiftBackApplication.class, args);
+    }
+
+}
